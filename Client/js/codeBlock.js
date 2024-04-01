587c737f8e4f25cd581675724a9df758c6f2a073
qazplm5602 <t01048187045@gmail.com>
2024-04-01 17:44:26 +0900

    코드블록 열었다가 말았다가

--- Client/js/codeBlock.js
+++ Client/js/codeBlock.js
@@ -2,10 +2,20 @@ const codeBlock = {
     waitHandler: undefined,
 
     show() {
+        if (this.waitHandler) clearTimeout(this.waitHandler);
         
+        $(".code_window").show();
+        this.waitHandler = setTimeout(() => {
+            $(".code_window").removeClass("hide");
+        }, 10);
     },
 
     hide() {
+        if (this.waitHandler) clearTimeout(this.waitHandler);
         
+        $(".code_window").addClass("hide");
+        this.waitHandler = setTimeout(() => {
+            $(".code_window").hide();
+        }, 250);
     }
 }

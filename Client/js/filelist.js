1dd306f09fe2f5683960eaddc654e17e66aaf0d8
domi <t01048187045@gmail.com>
2024-04-02 17:20:35 +0000

    파일 목록 추가 / 확장자에 따라 로고 지정

--- Client/js/filelist.js
+++ Client/js/filelist.js
@@ -1,5 +1,12 @@
 const filelist = {
-    currentPath: "/"
+    currentPath: "/",
+    fileExts: { // 지원하는 언어 (여기에 등록되어 있으면 아이콘이 바뀜)
+      "js": true,
+      "html": true,
+      "css": true,
+      "json": true,
+      "lua": true
+    }
 }
 
 $(() => filelist["$list"] = $(".file_container > .list"));
@@ -24,7 +31,26 @@ domiSocket.addEvent("file.directory.result", function(data) {
         } else return 0;
     });
 
+    // 해당 폴더 뒤로 갈 수 있음
+    if (data.path !== "/")
+      $list.append(`<div class="box folder">
+          <img src="./assets/extIco/folder.svg">
+          <span>../</span>
+      </div>`);
+
     $.each(data.files, function(i, v) {
-        
+      let ext;
+      let logo = v.directory ? "folder" : "file";
+      if (!v.directory && v.name.lastIndexOf('.') !== -1) {
+        ext = v.name.substring(v.name.lastIndexOf('.'));
+
+        if (filelist.fileExts[ext])
+          logo = ext;
+      }
+    
+      $list.append(`<div class="box ${v.directory ? "folder" : "file"}">
+          <img src="./assets/extIco/${logo}.svg">
+          <span>${v.name}</span>
+      </div>`);
     });
 });

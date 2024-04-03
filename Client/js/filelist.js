cddd3e6661236a45937c9501ae915256adbd146a
qazplm5602 <t01048187045@gmail.com>
2024-04-03 10:21:09 +0900

    폴더 표시

--- Client/js/filelist.js
+++ Client/js/filelist.js
@@ -9,14 +9,36 @@ const filelist = {
     }
 }
 
-$(() => filelist["$list"] = $(".file_container > .list"));
+$(() => {
+    filelist["$list"] = $(".file_container > .list");
+    
+    $(document).on("click", ".file_container > .list > .box", function() {
+        const directory = $(this).data("directory");
+        let name = $(this).data("name");
+    
+        /// 전으로 가라
+        if (directory) {
+            if (name === "../") {
+                const findSlash = filelist.currentPath.lastIndexOf('/');
+                name = findSlash === -1 ? "/" : filelist.currentPath.substring(0, findSlash);
+            } else {
+                name = `${filelist.currentPath === "" ? "" :  `${filelist.currentPath}/`}${name}`;
+            }
+            
+            domiSocket.send("file.request.directory", name);
+            return;
+        }
+
+        
+    });
+});
 
 domiSocket.addEvent("file.directory.result", function(data) {
     const $list = filelist.$list;
+
+    if (data.path === "/") data.path = "";
     filelist.currentPath = data.path;
     $list.empty();
-
-    console.log(data.files);
 
     // 정렬
     data.files = data.files.sort(function(a, b) {
@@ -32,8 +54,8 @@ domiSocket.addEvent("file.directory.result", function(data) {
     });
 
     // 해당 폴더 뒤로 갈 수 있음
-    if (data.path !== "/")
-      $list.append(`<div class="box folder">
+    if (data.path !== "")
+      $list.append(`<div data-directory=true data-name="../" class="box folder">
           <img src="./assets/extIco/folder.svg">
           <span>../</span>
       </div>`);
@@ -42,13 +64,13 @@ domiSocket.addEvent("file.directory.result", function(data) {
       let ext;
       let logo = v.directory ? "folder" : "file";
       if (!v.directory && v.name.lastIndexOf('.') !== -1) {
-        ext = v.name.substring(v.name.lastIndexOf('.'));
-
+        ext = v.name.substring(v.name.lastIndexOf('.') + 1);
+        
         if (filelist.fileExts[ext])
           logo = ext;
       }
     
-      $list.append(`<div class="box ${v.directory ? "folder" : "file"}">
+      $list.append(`<div data-directory=${v.directory} data-name="${v.name}" class="box ${v.directory ? "folder" : "file"}">
           <img src="./assets/extIco/${logo}.svg">
           <span>${v.name}</span>
       </div>`);

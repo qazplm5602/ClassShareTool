cf511c329b1b6ec09a499712af61241999caadac
qazplm5602 <t01048187045@gmail.com>
2024-04-08 18:04:15 +0900

    dialog 창 버튼 연동

--- Client/js/notify.js
+++ Client/js/notify.js
@@ -3,6 +3,15 @@ const notify = {
     waitHandler: undefined,
     show: function(title, content, buttons) {
         if (this.waitHandler) clearTimeout(this.waitHandler);
+
+        $(".notify_window .title").text(title);
+        $(".notify_window main").html(content);
+        
+        $(".notify_window .buttons").empty();
+        buttons.forEach((button, i) => {
+            $(".notify_window .buttons").append(`<button data-id=${i}>${button[0]}</button>`);
+            this.callback[i] = button[1];
+        });
         
         $(".notify_window").show();
         this.waitHandler = setTimeout(() => {
@@ -16,5 +25,13 @@ const notify = {
         this.waitHandler = setTimeout(() => {
             $(".notify_window").hide();
         }, 250);
+    },
+    click: function(idx) {
+        this.callback[idx]();
     }
 }
+
+$(document).on("click", ".notify_window .buttons > button", function() {
+    const idx = $(this).data("id");
+    notify.click(idx);
+});
